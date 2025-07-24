6e6c24110e7f5d8c430e91190cfb465817e1ed41
Sondos Amr <sa3178@fayoum.edu.eg>
2025-07-25 02:56:01 +0300

    fix img

--- src/pages/Main/Home.jsx
+++ src/pages/Main/Home.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import './Home.css';
 import { TypeAnimation } from 'react-type-animation';
 import { motion } from 'framer-motion';
-import profileImg from 'D:/portifolio/react-portfolio/src/assets/profile.jpg';
+// import profileImg from '/';
 
 const Home = () => {
     const sentence = "Computer and Systems Engineering graduate with experience in AI and data science. Skilled in deep learning and data analysis. Passionate about applying AI to real-world problems.";
     const words = sentence.split(" ");
     return (
         <div className="home-container">
-        <img src={profileImg} alt="Profile" className="profile-img" />
+        <img src="/WhatsApp Image 2025-07-24 at 00.13.15_0100e64e.jpg" alt="Profile" className="profile-img" />
         <h1 className="name-title">Sondos Amr</h1>
         <p className="job-title">Data Scientist & Data Analyst</p>
         <p className="about-me">
